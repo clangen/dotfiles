6adf652db115f51488da224994ce70bc6cc765ba
Casey Langen <casey.langen@gmail.com>
2020-05-30 13:19:16 -0700

    Put normalized files in a subdirectory for easier book keeping.

--- scripts/mono-downmix.sh
+++ scripts/mono-downmix.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 DB=`ffmpeg -i "$1" -af "volumedetect" -vn -sn -dn -f null /dev/null 2>&1 | grep max_volume | sed -n "s/\(.*max_volume: -\)\(.*\)\( dB\)/\2/p"`
 DB=`bc -l <<< "${DB} - 1"`
-OUT="${1%.*}-mono-downmix-normalized.mkv"
-OUT="${OUT// /-}"
+BASE="${1%.*}"
+BASE="${BASE// /-}"
+OUT="${BASE}/${BASE}-mono-downmix-normalized.mkv"
+mkdir -p ${BASE}
 echo "normalizing to -${DB} dB, downmixing to mono and writing to '${OUT}'..."
 ffmpeg -i "$1" -af "afftdn=nt=w:om=o,volume=${DB}dB" -ac 1 -c:v copy -c:a aac -b:a 192k "${OUT}" 
--- scripts/mono-right.sh
+++ scripts/mono-right.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 DB=`ffmpeg -i "$1" -af "volumedetect" -vn -sn -dn -f null /dev/null 2>&1 | grep max_volume | sed -n "s/\(.*max_volume: -\)\(.*\)\( dB\)/\2/p"`
 DB=`bc -l <<< "${DB} - 1.0"`
-OUT="${1%.*}-mono-right-normalized.mkv"
-OUT="${OUT// /-}"
+BASE="${1%.*}"
+BASE="${BASE// /-}"
+OUT="${BASE}/${BASE}-mono-right-normalized.mkv"
+mkdir -p ${BASE}
 echo "normalizing to -${DB} dB, downmixing to mono and writing to '${OUT}'..."
 ffmpeg -i "$1" -af "afftdn=nt=w:om=o,volume=${DB}dB,pan=mono|c0=FR" -c:v copy -c:a aac -b:a 192k "${OUT}"
